9e44bcf77361d808d532c7b4956961c9be68051a
aijh1501 <aijh1501@gmail.com>
2026-02-27 02:30:14 +0600

    Add configuration file for API keys and environment variables

---
+++ config.py
@@ -0,0 +1,8 @@
+# Configuration for API keys and environment variables
+
+API_KEY = 'your_api_key_here'
+API_SECRET = 'your_api_secret_here'
+
+# Add your environment variables below
+ENV_VARIABLE_1 = 'value1'
+ENV_VARIABLE_2 = 'value2'
